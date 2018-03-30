c0bdb295886ad8b46026c9f5534442d097317ac4
Tom Logan <talogan@alaska.edu>
2018-03-30 22:36:56 +0000

    Use logging

--- src/getUsernamePassword.py
+++ src/getUsernamePassword.py
@@ -1,17 +1,18 @@
 
 import os
 from os.path import expanduser
+import logging
 
 def getUsernamePassword():
     home = expanduser("~")
     fileName = home + '/.netrc'
     if not os.path.isfile(fileName):
-        print "ERROR: No .netrc file found in {}".format(home)
-        print ""
-        print "You will need a .netrc file in order to downoad the HyP3 data."
-        print "Please see the documentation or the webpage https://disc.gsfc.nasa.gov/data-access"
-        print "for information on how to create an Earthdata login and a .netrc file"
-        print ""
+        logging.debug("ERROR: No .netrc file found in {}".format(home))
+        logging.debug("")
+        logging.debug("You will need a .netrc file in order to downoad the HyP3 data.")
+        logging.debug("Please see the documentation or the webpage https://disc.gsfc.nasa.gov/data-access")
+        logging.debug("for information on how to create an Earthdata login and a .netrc file")
+        logging.debug("")
         exit(1)
     f = open(fileName,"r")
     username = None
@@ -19,12 +20,12 @@ def getUsernamePassword():
     for line in f:
         if "login" in line:
             username = line.split()[1]
-            print "Username is {}".format(username)
+            logging.info("Username is {}".format(username))
         if "password" in line:
             password = line.split()[1]
-            print "Found password"
+            logging.info("Found password")
     if username is None or password is None:
-        print "ERROR: Unable to get username or password from .netrc file"
+        logging.error("ERROR: Unable to get username or password from .netrc file")
         exit(1)
     return username, password
 
